53875cb45fbb8aef9621a00e10a4cc30bfd2e48b
talayhan <samet.talayhan@gmail.com>
2017-05-20 11:03:07 +0300

    [tools] add dnstop/vnstat/iftop tools

--- user_specific/tools.sh
+++ user_specific/tools.sh
@@ -1,40 +1,58 @@
 #!/bin/bash
 
-#Program Name                              | Little description if necessary               |
-#URL                                       | stalayhan.github.io                           |
-#----------------------------              | --------------------------------
-#Total number of program                   | 5                                             |
+#Program Name                                      | Little description if necessary                  |
+#URL                                               | stalayhan.github.io                              |
+#----------------------------                      | --------------------------------
+#Total number of program                           | 8                                                |
 
 if [ $(id -u) -ne 0 ]; then exec sudo $0; fi
 
-#Httpie                                    | HTTP Client                                   |
-#URL                                       | https://github.com/jakubroztocil/httpie#linux |
+#Httpie                                            | HTTP Client                                      |
+#URL                                               | https://github.com/jakubroztocil/httpie#linux    |
 if [[ ! $(which http) ]]; then
-	sudo apt-get install httpie
+	apt-get install httpie
 fi
 
-#Cmus                                      | Command line music player                     |
-#URL                                       | https://github.com/cmus/cmus                  |
+#Cmus                                              | Command line music player                        |
+#URL                                               | https://github.com/cmus/cmus                     |
 if [[ ! $(which cmus) ]]; then
 	apt-get install cmus
 fi
 
-#Grip                                      | Preview markdown files                        |
-#URL                                       | https://github.com/joeyespo/grip              |
+#Grip                                              | Preview markdown files                           |
+#URL                                               | https://github.com/joeyespo/grip                 |
 if [[ ! $(which grip) ]]; then
 	pip install --upgrade grip
 fi
 
-#fzf                                       | fuzzy finder                                  |
-#URL                                       | https://github.com/junegunn/fzf               |
+#fzf                                               | fuzzy finder                                     |
+#URL                                               | https://github.com/junegunn/fzf                  |
 if [[ ! -e ~/.fzf ]]; then
 	git clone --depth 1 \
 	https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
 fi
 
-#vifm                                      | vim like file manager                         |
-#URL                                       | https://github.com/vifm/vifm                  |
+#vifm                                              | vim like file manager                            |
+#URL                                               | https://github.com/vifm/vifm                     |
 if [[ ! $(which vifm) ]]; then
-	sudo apt-get install vifm
+	apt-get install vifm
+fi
+
+#dnstop                                            | display various tables of dns traffic            |
+#URL                                               | http://dns.measurement-factory.com/tools/dnstop/ |
+if [[ ! $(which dnstop) ]]; then
+	apt-get install dnstop
+fi
+
+#vnstat                                            | console-based network traffic monitor tool       |
+#URL                                               | http://humdi.net/vnstat/                         |
+if [[ ! $(which vnstat) ]]; then
+	apt-get install vnstat
+fi
+
+#iftop                                             | display bandwidth usage on an interface          |
+#URL                                               | http://www.ex-parrot.com/pdw/iftop/              |
+if [[ ! $(which dnstop) ]]; then
+	apt-get install iftop
 fi
