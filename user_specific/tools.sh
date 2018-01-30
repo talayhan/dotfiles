1cbd4896ba3fa514b83c18c6e805b21563d1fca1
talayhan <samet.talayhan@gmail.com>
2018-01-30 11:18:58 +0300

    [tools] add diagnostic messages and vim plug installation

--- user_specific/tools.sh
+++ user_specific/tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [ $(id -u) -ne 0 ]; then exec sudo "$0"; fi
 
 tools=(
@@ -20,23 +24,41 @@ tools=(
 )
 
 for tool in "${tools[@]}" ; do
+	echo "${GREEN}[+] Installing ${tool} ... ${NC}"
 	if [[ ! $(which "$tool") ]]; then
 		apt-get install "$tool"
 	fi
+	echo "${GREEN}[+] Done${NC}"
 done
 
 if [[ ! $(which grip) ]]; then
+	echo "${GREEN}[+] Installing grip ... ${NC}"
 	pip install --upgrade grip
+	echo "${GREEN}[+] Done${NC}"
 fi
 
 if [[ ! -e ~/.fzf ]]; then
+	echo "${GREEN}[+] Installing fzf ... ${NC}"
 	git clone --depth 1 \
 	https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install
+	echo "${GREEN}[+] Done ${NC}"
 fi
 
 if [[ ! -e ~/.oh-my-zsh ]]; then
+	echo "${GREEN}[+] Installing oh-my-zsh ... ${NC}"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	echo "${GREEN}[+] Done ${NC}"
+fi
+
+# install vim plugin manager
+if [[ -e ~/.vim && ! -e ~/.vim/plugged ]]; then
+	echo "${GREEN}[+] Installing vim plugin manager ... ${NC}"
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	echo "${GREEN}[+] Done ${NC}"
+else
+	echo "${RED}[-] You need to install Vim first!${NC}"
 fi
 
 SOURCE_STR="
