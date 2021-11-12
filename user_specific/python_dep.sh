3cc40138cd1c0257411133d925b84b5db1e17ed3
talayhan <samet.talayhan@gmail.com>
2021-11-12 12:04:25 +0300

    Separate installation scripts

---
+++ user_specific/python_dep.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install pyx
+pip install pyx
+
+# install pynvim for neovim
+python3 -m pip install --user --upgrade pynvim
---
+++ user_specific/rust_dep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://sh.rustup.rs -sSf | sh
+
+cargo install alacritty
