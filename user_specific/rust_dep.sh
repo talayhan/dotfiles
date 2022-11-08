0506adc170715558b5c5e96c1f6d90894fda3611
talayhan <samet.talayhan@gmail.com>
2022-11-08 17:02:12 +0300

    [rust_dep] Update rust package installer script

--- user_specific/rust_dep.sh
+++ user_specific/rust_dep.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+source ".logs.sh"
+
 curl https://sh.rustup.rs -sSf | sh
 
-cargo install alacritty
-cargo install git-delta
+pkgs=(
+'alacritty'
+'git-delta'
+'cargo-bloat' #find out what takes most of the space in your executable
+)
+
+for package in "${pkgs[@]}" ; do
+    if [[ ! $(which "$package") ]]; then
+        debug_log "[+] Installing ${package} ... "
+        cargo install "$package"
+        debug_log "[+] Done "
+    fi
+done
+
