b8fbac984d0765a321c115e1d8bc1ce975d6d953
talayhan <samet.talayhan@gmail.com>
2024-03-21 14:29:16 -0700

    Add command to toggle the diagnostic warnings

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -63,3 +63,13 @@ require('lspconfig')['clangd'].setup {
   on_attach = on_attach,
   settings = {}
 }
+
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
