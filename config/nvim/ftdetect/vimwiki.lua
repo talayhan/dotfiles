d270acffa9cf3a8baafa9f63b170ad94ec2542ed
talayhan <samet.talayhan@gmail.com>
2024-03-21 14:29:30 -0700

    Add vimwiki.lua to fix markdown issue
    ref: https://github.com/vimwiki/vimwiki/issues/1384

---
+++ config/nvim/ftdetect/vimwiki.lua
@@ -0,0 +1,9 @@
+--vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown'}
+vim.g.vimwiki_global_ext = 0
+vim.g.vimwiki_list = {
+  {
+    path = '~/vimwiki/',
+    syntax = 'markdown',
+    ext = '.md'
+  }
+}
