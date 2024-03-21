28c358a5581992825ab07a29fa2bc76e8a98ca98
talayhan <samet.talayhan@gmail.com>
2024-03-21 15:36:00 -0700

    [nvim] Update vimwiki config

--- config/nvim/ftdetect/vimwiki.lua
+++ config/nvim/ftdetect/vimwiki.lua
@@ -1,4 +1,6 @@
---vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown'}
+vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown'}
+vim.g.vimwiki_filetypes = { "markdown" }
+vim.g.vimwiki_markdown_link_ext = 1
 vim.g.vimwiki_global_ext = 0
 vim.g.vimwiki_list = {
   {
