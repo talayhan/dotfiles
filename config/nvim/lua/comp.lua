2f0973271ea0fdfbbd0e4b075ac59d6a4b8f63ca
talayhan <samet.talayhan@gmail.com>
2024-04-29 13:37:16 -0700

    [nvim] Change LSP autocomplete key

--- config/nvim/lua/comp.lua
+++ config/nvim/lua/comp.lua
@@ -38,7 +38,7 @@ cmp.setup {
                 fallback()
             end
         end, { 'i', 's' }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-b>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -47,7 +47,7 @@ cmp.setup {
                 fallback()
             end
         end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        ['<C-p>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable( -1) then
