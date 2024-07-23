50a32bfd0bc4c308d4bcf58729f0adaa423754a0
talayhan <samet.talayhan@gmail.com>
2024-07-23 11:11:16 -0700

    [nvim] Don't check treesitter sync

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -214,9 +214,9 @@ require('lazy').setup({
         dependencies = {
             'nvim-treesitter/nvim-treesitter-textobjects',
         },
-        config = function()
-            pcall(require('nvim-treesitter.install').update { with_sync = true })
-        end,
+        --config = function()
+            --pcall(require('nvim-treesitter.install').update { with_sync = true })
+        --end,
     },
     {
         "folke/todo-comments.nvim",
