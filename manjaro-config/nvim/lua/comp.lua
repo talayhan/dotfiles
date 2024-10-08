d55cf33d04a63f85941cfdb7a1486a7279532065
talayhan <samet.talayhan@gmail.com>
2024-10-08 00:32:09 -0700

    [manjaro-config] Update comp keyb for snip

--- manjaro-config/nvim/lua/comp.lua
+++ manjaro-config/nvim/lua/comp.lua
@@ -19,14 +19,14 @@ cmp.setup {
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = false,
         },
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-o>'] = cmp.mapping.complete(),
         ['<C-A>'] = cmp.mapping(function(fallback)
             if luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
